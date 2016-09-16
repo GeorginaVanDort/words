1590b76fddf9c159ed1c5e2a0cec9cd8d1d6320b
Georgina Van Dort <georginavandort@gmail.com>
2016-09-16 08:39:36 -0700

    more tests

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -3,10 +3,21 @@ import java.util.ArrayList;
 
 public class Word {
   private String mText;
+  private static ArrayList<Word> allWords = new ArrayList<Word>();
 
 public Word(String text) {
   mText = text;
+  allWords.add(this);
 }
+
+public String getText() {
+  return mText;
+  }
+
+public static ArrayList<Word> all() {
+  return allWords;
+}
+
 
 
 }
--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -6,9 +6,23 @@ import java.util.List;
 public class WordTest{
 
   @Test
-   public void word_instantiatesCorrectly_true() {
+   public void word_instantiates_true() {
      Word testWord = new Word("hello");
      assertEquals(true, testWord instanceof Word);
    }
+
+   @Test
+    public void word_instantiatesWithTextValue_true() {
+      Word testWord = new Word("hello");
+      assertEquals("hello", testWord.getText());
+    }
+
+    @Test
+    public void all_returnsAllWords_true() {
+      Word testWord = new Word("Hello");
+      Word testWord2 = new Word("Goodbye");
+      assertEquals(true, Word.all().contains(testWord));
+      assertEquals(true, Word.all().contains(testWord2));
+    }
 
 }
