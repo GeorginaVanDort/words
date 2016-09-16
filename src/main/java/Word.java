9222686a3707983427ad936ae369128c701a2b71
Georgina Van Dort <georginavandort@gmail.com>
2016-09-16 08:50:03 -0700

    id functionality for words

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 public class Word {
   private String mText;
   private static ArrayList<Word> allWords = new ArrayList<Word>();
+  private int mId;
+
 
 public Word(String text) {
   mText = text;
   allWords.add(this);
+  mId = allWords.size();
 }
 
 public String getText() {
@@ -18,6 +21,16 @@ public static ArrayList<Word> all() {
   return allWords;
 }
 
+public static void clear () {
+  allWords.clear();
+}
 
+public int getId() {
+  return mId;
+}
+
+public static Word find(int id) {
+ return allWords.get(id - 1);
+}
 
 }
--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -6,23 +6,44 @@ import java.util.List;
 public class WordTest{
 
   @Test
-   public void word_instantiates_true() {
-     Word testWord = new Word("hello");
-     assertEquals(true, testWord instanceof Word);
-   }
+  public void word_instantiates_true() {
+    Word testWord = new Word("hello");
+    assertEquals(true, testWord instanceof Word);
+  }
 
-   @Test
-    public void word_instantiatesWithTextValue_true() {
-      Word testWord = new Word("hello");
-      assertEquals("hello", testWord.getText());
-    }
+  @Test
+  public void word_instantiatesWithTextValue_true() {
+    Word testWord = new Word("hello");
+    assertEquals("hello", testWord.getText());
+  }
 
-    @Test
-    public void all_returnsAllWords_true() {
-      Word testWord = new Word("Hello");
-      Word testWord2 = new Word("Goodbye");
-      assertEquals(true, Word.all().contains(testWord));
-      assertEquals(true, Word.all().contains(testWord2));
-    }
+  @Test
+  public void all_returnsAllWords_true() {
+    Word testWord = new Word("Hello");
+    Word testWord2 = new Word("Goodbye");
+    assertEquals(true, Word.all().contains(testWord));
+    assertEquals(true, Word.all().contains(testWord2));
+  }
+
+  @Test
+  public void clear_emptiesAllWords_0() {
+    Word testWord = new Word("Hello");
+    Word testWord2 = new Word("Goodbye");
+    Word.clear();
+    assertEquals(Word.all().size(), 0);
+  }
+  @Test
+    public void getId_wordsInstantiateWithAnId_1() {
+    Word testWord = new Word("Hello");
+    assertEquals(1, testWord.getId());
+  }
+
+  @Test
+    public void find_returnsWordWithSameId_secondWord() {
+    Word.clear();
+    Word testWord = new Word("Hello");
+    Word testWord2 = new Word("Goodbye");
+    assertEquals(Word.find(testWord2.getId()), testWord2);
+  }
 
 }
